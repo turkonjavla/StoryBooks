4169d9548e0d2d6c8b1ce5c18a957e5f71bb9736
turkonjavla <turkonjavlada@gmail.com>
2018-03-10 20:19:10 +0100

    Implement user creation with google oauth

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,9 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy,
       mongoose       = require("mongoose"),
       keys           = require("./keys");
 
+// MODELS
+const User = require("../models/User");
+
 module.exports = function(passport) {
     passport.use(
         new GoogleStrategy({
@@ -10,8 +13,34 @@ module.exports = function(passport) {
             callbackURL: "/auth/google/callback",
             proxy: true
         }, (accessToken, resfreshToken, profile, done) => {
-            console.log(accessToken);
-            console.log(profile);
+            /* console.log(accessToken);
+            console.log(profile); */
+            const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf("?"));
+            
+            const newUser = {
+                googleID: profile.id,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                email: profile.emails[0].value,
+                image: image
+            }
+
+            User.findOne({
+                googleID: profile.id
+            })
+                .then(user => {
+                    if(user) {
+                        // return user
+                        done(null, user);
+                    }
+                    else {
+                        // create user
+                        User.create(newUser)
+                            .then(user => {
+                                done(null, user);
+                            });
+                    }
+                });
         })
     )
 }
