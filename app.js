25d9ce95f45155ba912af81dfe0fa32e71cd4d10
turkonjavla <turkonjavlada@gmail.com>
2018-03-10 19:09:54 +0100

    Add inital passport config

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const mongoose = require("mongoose"),
+      passport = require("passport"),
       express  = require("express"),
       app      = express(),
       port     = process.env.PORT || 5050;
 
 // DB connection
-const db = process.env.DATABASEURL;
+/* const db = process.env.DATABASEURL;
 mongoose.connect(db)
     .then(() => consolellog("MongoDB connected!"))
     .catch(err => console.log(`Error ${err}`));
+ */
+
+ // PASSPORT CONFIG
+ require(".//config/passport")(passport);
+
+// LOAD ROUTES
+const auth = require("./routes/auth");
 
 app.get("/", (req, res) => {
     res.send("IT WORKS");
 });
+
+// ROUTES
+app.use("/auth", auth);
 
 app.listen(port, process.env.IP, () => {
     console.log(`Server open at port: ${port}`);
---
+++ config/passport.js
@@ -0,0 +1,17 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy,
+      mongoose       = require("mongoose"),
+      keys           = require("./keys");
+
+module.exports = function(passport) {
+    passport.use(
+        new GoogleStrategy({
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecet,
+            callback: "/auth/google/callback",
+            proxy: true
+        }, (accessToken, resfreshToken, profile, done) => {
+            console.log(accessToken);
+            console.log(profile);
+        })
+    )
+}
