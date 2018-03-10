95da238661ffdc50d5bd75353eddc0a778bada48
turkonjavla <turkonjavlada@gmail.com>
2018-03-10 19:53:38 +0100

    Update app.js Add db connection from keys.js file

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose"),
       passport = require("passport"),
       express  = require("express"),
+      keys     = require("./config/keys"),
       app      = express(),
       port     = process.env.PORT || 5050;
 
 // DB connection
-/* const db = process.env.DATABASEURL;
+const db = keys.mongoURI;
 mongoose.connect(db)
-    .then(() => consolellog("MongoDB connected!"))
+    .then(() => console.log("MongoDB connected!"))
     .catch(err => console.log(`Error ${err}`));
- */
+ 
 
  // PASSPORT CONFIG
  require(".//config/passport")(passport);
