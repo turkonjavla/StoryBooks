666569de804a5147f5c658ba65790ec0b1c678b6
turkonjavla <turkonjavlada@gmail.com>
2018-03-10 19:38:09 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,16 @@ const passport = require("passport"),
       express = require("express"),
       router = express.Router();
 
-router.get("/google", passport.authenticate("google", {scope: ["profile", "email"]}));
+router.get("/google", passport.authenticate("google", 
+    {scope: [
+        "profile", "email"
+    ]
+}));
+
+router.get("/google/callback", passport.authenticate("google", {
+    failureReddirect: "/"
+}), (req, res) => {
+    res.redirect("/dashboard");
+});
 
 module.exports = router;
