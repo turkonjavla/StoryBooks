8fe841e26e47afd9bcf0873fd8f2a85022bb9f09
turkonjavla <turkonjavlada@gmail.com>
2018-03-11 01:21:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 });
 
  // PASSPORT CONFIG
- require(".//config/passport")(passport);
+ require("./config/passport")(passport);
 
 // HANDLEBARS
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
