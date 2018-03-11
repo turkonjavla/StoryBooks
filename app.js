138ddc91c7524c0475c1c31b35a8963482762be1
turkonjavla <turkonjavlada@gmail.com>
2018-03-11 14:57:35 +0100

    Update date in footer to use moment.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
     res.locals.user = req.user || null;
     next();
 });
-app.locals.moment = moment;
+app.locals.moment = moment(new Date).format("YYYY");
 
  // PASSPORT CONFIG
  require("./config/passport")(passport);
