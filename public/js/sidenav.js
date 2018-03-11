8c2dd95440fb8eabd48b50399fee8f9b28c813e6
turkonjavla <turkonjavlada@gmail.com>
2018-03-11 15:50:26 +0100

    Add CKEditor

--- public/js/sidenav.js
+++ public/js/sidenav.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
     $(".button-collapse").sideNav();
 });
+
+$(document).ready(function() {
+    $('select').material_select();
+});
+
+CKEDITOR.replace("body", {
+    plugins: "wysiwygarea,toolbar,basicstyles,link"
+});
